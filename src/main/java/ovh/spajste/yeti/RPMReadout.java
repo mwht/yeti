0564799af05a8cfba11c4f2f30fea0dccefa2f7d
Sebastian Madejski <madejski.s97@gmail.com>
2018-05-09 17:51:59 +0200

    add RPMReadout stub

---
+++ src/main/java/ovh/spajste/yeti/RPMReadout.java
@@ -0,0 +1,22 @@
+package ovh.spajste.yeti;
+
+public class RPMReadout extends Readout {
+
+    public RPMReadout() {
+        name = "RPM";
+        value = 0;
+        pid = "0C";
+    }
+
+    public void calculateValue() {
+
+    }
+
+    public int getExpectedBytes() {
+        return 2;
+    }
+
+    public int getExpectedMode() {
+        return 4;
+    }
+}
