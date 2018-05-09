e36760b24c78aef208944e90a5ab4786114359c4
Lukkario <michallukianski.0@gmail.com>
2018-05-09 22:15:26 +0200

    Added selectPort method Added openConnection method Added closeConnection method Added readData method Added sendData method

--- src/main/java/ovh/spajste/yeti/SerialCommunication.java
+++ src/main/java/ovh/spajste/yeti/SerialCommunication.java
@@ -27,4 +27,51 @@ public class SerialCommunication {
         return portNames.toArray(new String[0]);
     }
 
+    public SerialPort selectPort(String portName)
+    {
+        for(SerialPort port: availablePorts)
+        {
+            if (portName.equals(port.getSystemPortName()))
+                return port;
+        }
+        return null;
+    }
+
+
+    public void openConnection(SerialPort port, int boud)
+    {
+        port.setBaudRate(boud);
+        port.openPort();
+    }
+
+    public void closeConnection(SerialPort port)
+    {
+        if(port.isOpen())
+            port.closePort();
+    }
+
+    public byte[] readData(SerialPort port, long amountOfBytesToRead)
+    {
+        if(!port.isOpen())
+        {
+            /*
+                TODO: Throw exception about not open port
+
+             */
+        }
+        byte[] bytesToRead = new byte[(int)amountOfBytesToRead];
+        port.readBytes(bytesToRead, amountOfBytesToRead);
+        return bytesToRead;
+
+    }
+
+    public void sendData(SerialPort port, byte[] dataToBeSend,long amountOfBytesToSend)
+    {
+        if(port.writeBytes(dataToBeSend, amountOfBytesToSend) == -1)
+        {
+            /*
+                TODO: Throw exception about not sucessfuly send data
+             */
+        }
+    }
 }
