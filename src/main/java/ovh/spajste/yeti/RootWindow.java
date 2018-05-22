51cfa7cd0977e17f28b052cf2970fc48709b29cc
Lukkario <michallukianski.0@gmail.com>
2018-05-23 00:50:50 +0200

    Cleanup

--- src/main/java/ovh/spajste/yeti/RootWindow.java
+++ src/main/java/ovh/spajste/yeti/RootWindow.java
@@ -1,6 +1,5 @@
 package ovh.spajste.yeti;
 
-import com.fazecast.jSerialComm.SerialPort;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -8,7 +7,6 @@ import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
-import java.util.Arrays;
 
 public class RootWindow extends Application {
     public static SerialCommunication serial = new SerialCommunication();
@@ -19,15 +17,6 @@ public class RootWindow extends Application {
     @Override
     public void start(Stage primaryStage) {
         try {
-            //byte[] at = new byte[] {0x41,0x54,0x5a,0xa};//{0x30,0x31,0x30,0x30,0xa};//{0x41,0x54,0x5a,0xa};
-            //SerialPort communicationPort = serial.selectPort("ttyUSB0");
-            //serial.openConnection(communicationPort,38400);
-
-            //serial.sendData(communicationPort,at);
-            //byte[] response = serial.waitAndReadData(communicationPort);
-            //System.out.println(new String(response));
-            //serial.closeConnection(communicationPort);
-
             AnchorPane root = FXMLLoader.load(RootWindow.class.getResource("/fxml/RootWindow.fxml"));
             primaryStage.setScene(new Scene(root, 700, 600));
             primaryStage.setTitle("Yeti by SpajsTech Ltd. 2018");
