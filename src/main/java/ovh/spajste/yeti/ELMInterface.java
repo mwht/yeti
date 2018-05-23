3845c77cea9cc6cfc711a2ca97a0612c023bcfd0
Sebastian Madejski <madejski.s97@gmail.com>
2018-05-23 15:26:02 +0200

    add *theoritical* pid readout

--- src/main/java/ovh/spajste/yeti/ELMInterface.java
+++ src/main/java/ovh/spajste/yeti/ELMInterface.java
@@ -4,7 +4,9 @@ import com.fazecast.jSerialComm.SerialPort;
 
 import javax.xml.bind.DatatypeConverter;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 /**
@@ -26,7 +28,7 @@ public class ELMInterface {
     private SerialCommunication serialCommunication;
     private SerialPort serialPort;
     private String selectedPort;
-    private Thread receiveDataThread;
+    private Thread readoutDispatchThread;
     private enum Protocol {
         AUTOMATIC,
         SAE_J1850_PWM,
@@ -72,17 +74,38 @@ public class ELMInterface {
             String fullResponse = new String(serialCommunication.waitAndReadData(serialPort));
             if(Pattern.matches("/FAIL/",fullResponse)) {
                 // TODO: iterate through all protocol until success (@mwht)
-            } else if (Pattern.matches("^AUTO, (.*)",fullResponse)) {
-
+            } else {
+                Matcher match = Pattern.compile("^AUTO, (.*)").matcher(fullResponse);
+                if(match.groupCount() > 0) {
+                    currentProtocol = match.group(1);
+                } else {
+                    throw new Exception("Cannot initialize ELM interface - no matching protocol found");
+                }
             }
 
+            // --- read down all PIDs ---
+            serialCommunication.sendData(serialPort,"0100\n".getBytes());
+            serialCommunication.waitAndReadData(serialPort); // INIT...
+            serialCommunication.waitAndReadData(serialPort); // OK/FAIL
+
+            // TODO: readout all pids
+            /*byte[] pidRawData = convertELMdataToByteArray(new String(serialCommunication.waitAndReadData(serialPort)));
+            if(((pidRawData[0] & 0xF0) >> 4) == 4 && pidRawData[1] == 0x00) {
+                if(pidRawData.length == 6) {
+                    byte[] pidArray = Arrays.copyOfRange(pidRawData,2,6);
+                    int pids = (pidArray[0] << 24) | (pidArray[1] << 16) | (pidArray[2] << 8) | pidArray[3];
+
+                }
+            } else {
+                System.err.println("fail");
+            }*/
+            serialCommunication.waitAndReadData(serialPort);
         } catch(Exception e) {
             System.err.println("Exception caught in ELMInterface: "+e.getClass().getName()+" - "+e.getMessage());
         }
     }
 
     public static byte[] convertELMdataToByteArray(String s) {
-
         return DatatypeConverter.parseHexBinary(s.replace(" ",""));
     }
 
