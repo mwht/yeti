d360b4a29d8f066a24088038135fbdaaf84a2df5
Adam Sylla <adamski126@gmail.com>
2018-05-23 15:19:46 +0300

    add vehicle speed readoutclass

---
+++ src/main/java/ovh/spajste/yeti/VehicleSpeedReadout.java
@@ -0,0 +1,25 @@
+package ovh.spajste.yeti;
+
+public class VehicleSpeedReadout extends Readout {
+
+    public VehicleSpeedReadout() {
+        name = "Vehicle Speed";
+        value = 0;
+        pid = 0x0D;
+        unit = "km/h";
+    }
+
+    public void calculateValue() {
+        value = (readoutBuffer[0]);
+    }
+
+    public int getExpectedBytes() {
+        return 1;
+    }
+
+    public int getExpectedMode() {
+        return 0x41;
+    }
+}
+
+
