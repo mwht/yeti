e644db262f95cce05f2e5cedb490689e5ab7bdba
Adam Sylla <adamski126@gmail.com>
2018-05-23 15:10:18 +0300

    add short term fuel trim bank 1 readoutclass

---
+++ src/main/java/ovh/spajste/yeti/ShortTermFuelTrimBank1Readout.java
@@ -0,0 +1,23 @@
+package ovh.spajste.yeti;
+
+public class ShortTermFuelTrimBank1Readout extends Readout {
+
+    public ShortTermFuelTrimBank1Readout() {
+        name = "Short Term Fuel Trim Bank 1";
+        value = 0;
+        pid = 0x06;
+        unit = "%";
+    }
+
+    public void calculateValue() {
+        value = ((readoutBuffer[0]/1.28)-100);
+    }
+
+    public int getExpectedBytes() {
+        return 1;
+    }
+
+    public int getExpectedMode() {
+        return 0x41;
+    }
+}
