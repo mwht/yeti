6595d12f15342032e394dcab46536d12032187aa
Adam Sylla <adamski126@gmail.com>
2018-05-23 15:23:49 +0300

    add timing advance readoutclass

---
+++ src/main/java/ovh/spajste/yeti/TimingAdvanceReadout.java
@@ -0,0 +1,25 @@
+package ovh.spajste.yeti;
+
+public class TimingAdvanceReadout extends Readout {
+
+    public TimingAdvanceReadout() {
+        name = "Timing Advance";
+        value = 0;
+        pid = 0x0E;
+        unit = "°";
+    }
+
+    public void calculateValue() {
+        value = ((readoutBuffer[0]/2)-64);
+    }
+
+    public int getExpectedBytes() {
+        return 1;
+    }
+
+    public int getExpectedMode() {
+        return 0x41;
+    }
+}
+
+
