a61fe4c26fd09f7741373b9f9bf8ee841ed1b51f
Adam Sylla <adamski126@gmail.com>
2018-05-23 15:17:10 +0300

    add intake mainfold absolute pressure readoutclass

---
+++ src/main/java/ovh/spajste/yeti/IntakeMainfoldAbsolutePressureReadout.java
@@ -0,0 +1,24 @@
+package ovh.spajste.yeti;
+
+public class IntakeMainfoldAbsolutePressureReadout extends Readout {
+
+    public IntakeMainfoldAbsolutePressureReadout() {
+        name = "Intake Mainfold Absolute Pressure";
+        value = 0;
+        pid = 0x0B;
+        unit = "kPa";
+    }
+
+    public void calculateValue() {
+        value = (readoutBuffer[0]);
+    }
+
+    public int getExpectedBytes() {
+        return 1;
+    }
+
+    public int getExpectedMode() {
+        return 0x41;
+    }
+}
+
