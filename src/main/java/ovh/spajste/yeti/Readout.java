045b2a852adc54ac31fb3c91207d9c6f6ab7875d
Sebastian Madejski <madejski.s97@gmail.com>
2018-05-24 15:31:56 +0200

    finish Javadoc for Readout class

--- src/main/java/ovh/spajste/yeti/Readout.java
+++ src/main/java/ovh/spajste/yeti/Readout.java
@@ -34,32 +34,68 @@ public abstract class Readout {
     }
 
     /**
-     *
+     * Calculate the value from given readout buffer.
      */
     public abstract void calculateValue();
+    
+    /**
+     * Gets the number of bytes expected by readout.
+     * @return number of bytes expected by readout
+     */
     public abstract int getExpectedBytes();
+    
+    /**
+     * Gets the number of mode expectedy by readout
+     * @return number of mode expected by readout
+     */
     public abstract int getExpectedMode();
 
+    /**
+     * Gets the name of readout.
+     * @return name of readout
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Set the name of readout.
+     * @param name name of readout
+     */
     public void setName(String name) {
         this.name = name;
     }
 
+    /**
+     * Set the readout buffer. Used internally in {@link ELMInterface}.
+     * @param readoutBuffer buffer
+     */
     public void setReadoutBuffer(byte[] readoutBuffer) {
         this.readoutBuffer = readoutBuffer;
     }
 
+    /**
+     * Gets the unit which the readout returns the value in.
+     * @return unit of readout
+     */
     public String getUnit() {
         return unit;
     }
 
+    /**
+     * Sets the unit which the readout returns the value in.
+     * @param unit unit of readout
+     */
     public void setUnit(String unit) {
         this.unit = unit;
     }
 
+    /**
+     * Gets the value of readout. Throws {@link InvalidReadoutException}
+     * 
+     * @return value of readout
+     * @throws InvalidReadoutException
+     */
     public double getValue() throws InvalidReadoutException {
         if(readoutBuffer.length != getExpectedBytes()) {
             throw new InvalidReadoutException("Invalid number of bytes (readout type mismatch?)");
@@ -68,15 +104,28 @@ public abstract class Readout {
         return value;
     }
 
+    /**
+     * Gets the PID of readout itself.
+     * 
+     * @return readout's PID
+     */
     public byte getPid() {
         return pid;
     }
 
+    /**
+     * Sets whether readout should be polled by {@link ELMInterface}.
+     * @param active activity status of readout
+     */
     public void setActive(boolean status)
     {
         active = status;
     }
 
+    /**
+     * Gets whether readout should be polled by {@link ELMInterface}.
+     * @return activity status of readout
+     */
     public boolean getActive()
     {
         return active;
