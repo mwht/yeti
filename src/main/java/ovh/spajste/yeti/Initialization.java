c1d9071e7e42271f7e9c61e560da07b7f88b0d98
Lukkario <michallukianski.0@gmail.com>
2018-05-29 13:44:09 +0200

    Changes after consultation with @mwht

--- src/main/java/ovh/spajste/yeti/Initialization.java
+++ src/main/java/ovh/spajste/yeti/Initialization.java
@@ -8,15 +8,14 @@ public abstract class Initialization {
 
     protected String protocolName;
     protected String protocolSelectSequence;
+    protected SerialCommunication serialCommunication;
+    protected SerialPort serialPort;
 
     public ArrayList<Byte> availablePIDS = new ArrayList<>();
 
     private byte[] initializeSequence = {0x01,0x00,0x0A}; // 0100\n
     private byte[] elmResetSequence = {0x41,0x54,0x20,0x5A,0x0A}; // AT Z\n
     private String elmIdentificator = "Unknown";
-    private SerialCommunication serialCommunication;
-    private SerialPort serialPort;
-    static private byte[] listOfAllPIDS = {0x01,0x02,0x03,0x04,0x05,0x06,0x07,0x08,0x09,0x0A,0x0B,0x0C,0x0D,0x0E,0x0F,0x10,0x11,0x12,0x13,0x14,0x15,0x16,0x17,0x18,0x19,0x1A,0x1B,0x1C,0x1D,0x1E,0x1F,0x20};
 
     Initialization(SerialPort serialPort, SerialCommunication serialCommunication)
     {
@@ -24,6 +23,9 @@ public abstract class Initialization {
         protocolSelectSequence = "Unknown";
         this.serialPort = serialPort;
         this.serialCommunication = serialCommunication;
+    }
+
+    Initialization() {
     }
 
     public abstract void reciveProtocolSelectAnswer() throws InitializationException;
@@ -40,7 +42,7 @@ public abstract class Initialization {
                 }
                 if((pids[j] & (1<<i)) == 1)
                 {
-                    availablePIDS.add(listOfAllPIDS[i]);
+                    availablePIDS.add((byte) i);
                 }
             }
     }
