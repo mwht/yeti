d3e035cd03eaaa3d6ab56e6ded2548c0385eee01
Lukkario <michallukianski.0@gmail.com>
2018-05-29 13:12:38 +0200

    Finished extractAvailablePIDS method in Initialization abstract class

--- src/main/java/ovh/spajste/yeti/Initialization.java
+++ src/main/java/ovh/spajste/yeti/Initialization.java
@@ -16,6 +16,7 @@ public abstract class Initialization {
     private String elmIdentificator = "Unknown";
     private SerialCommunication serialCommunication;
     private SerialPort serialPort;
+    static private byte[] listOfAllPIDS = {0x01,0x02,0x03,0x04,0x05,0x06,0x07,0x08,0x09,0x0A,0x0B,0x0C,0x0D,0x0E,0x0F,0x10,0x11,0x12,0x13,0x14,0x15,0x16,0x17,0x18,0x19,0x1A,0x1B,0x1C,0x1D,0x1E,0x1F,0x20};
 
     Initialization(SerialPort serialPort, SerialCommunication serialCommunication)
     {
@@ -30,7 +31,18 @@ public abstract class Initialization {
 
     private void extractAvailablePIDS(byte[] pids)
     {
-        // TODO: finish this.
+            int j = 0;
+            for (int i = 0; i < 32; i++)
+            {
+                if(j % 8 == 0 && j != 0)
+                {
+                    j++;
+                }
+                if((pids[j] & (1<<i)) == 1)
+                {
+                    availablePIDS.add(listOfAllPIDS[i]);
+                }
+            }
     }
 
     private void resetElm() throws SerialSendDataException, PortNotOpenException {
