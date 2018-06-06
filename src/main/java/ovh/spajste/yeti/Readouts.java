1ebbd58df80206bc2b0b37c7b06617dc707d3ba2
Adam Sylla <adamski126@gmail.com>
2018-06-06 15:00:36 +0200

    fill in all readouts to HashMap in Readouts class

--- src/main/java/ovh/spajste/yeti/Readouts.java
+++ src/main/java/ovh/spajste/yeti/Readouts.java
@@ -13,5 +13,14 @@ public class Readouts {
     public static void init() {
         readoutMap = new HashMap<>();
         readoutMap.put((byte) 0x0C,RPMReadout.class);
+        readoutMap.put((byte) 0x04,CalculatedEngineLoadReadout.class);
+        readoutMap.put((byte) 0x05,EngineCoolantTemperatureReadout.class);
+        readoutMap.put((byte) 0x0F,IntakeAirTemperatureReadout.class);
+        readoutMap.put((byte) 0x0B,IntakeMainfoldAbsolutePressureReadout.class);
+        readoutMap.put((byte) 0x07,LongTermFuelTrimBank1Readout.class);
+        readoutMap.put((byte) 0x06,ShortTermFuelTrimBank1Readout.class);
+        readoutMap.put((byte) 0x11,ThrottlePositionReadout.class);
+        readoutMap.put((byte) 0x0E,TimingAdvanceReadout.class);
+        readoutMap.put((byte) 0x0D,VehicleSpeedReadout.class);
     }
 }
