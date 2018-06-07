0195cb31035e52080ae23626dbf40bc4133c4086
Sebastian Madejski <madejski.s97@gmail.com>
2018-06-07 17:06:56 +0200

    add *EXTREMELY* stub support for charts

--- src/main/java/ovh/spajste/yeti/RootWindow.java
+++ src/main/java/ovh/spajste/yeti/RootWindow.java
@@ -3,6 +3,8 @@ package ovh.spajste.yeti;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.XYChart;
 import javafx.scene.control.*;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
@@ -15,6 +17,7 @@ public class RootWindow extends Application {
     public static SerialCommunication serial = new SerialCommunication();
     public static ELMInterface elmInterface;
     public static AnchorPane root;
+    public static int cycle = 0;
 
     public static void main(String[] args) {
         launch(args);
@@ -29,8 +32,8 @@ public class RootWindow extends Application {
             primaryStage.setScene(scene);
             primaryStage.setTitle("Yeti by SpajsTech Ltd. 2018");
             primaryStage.show();
-            elmInterface = new ELMInterface();
-            elmInterface.initialize("ttyUSB0");
+            //elmInterface = new ELMInterface();
+            //elmInterface.initialize("ttyUSB0");
             Task readoutTask = new Task<Void>() {
 
 				@Override
@@ -59,8 +62,33 @@ public class RootWindow extends Application {
             	
             };
             Label rpmlabel = (Label) scene.lookup("#"+ThrottlePositionReadout.class.getSimpleName()+"Value");
-            new Thread(readoutTask).start();
+            //new Thread(readoutTask).start();
     	    rpmlabel.textProperty().bind(readoutTask.messageProperty());
+
+    	    LineChart line = (LineChart) scene.lookup("#readoutsChart");
+    	    line.getXAxis().setLabel("Time [ms]");
+    	    line.getYAxis().setLabel("Value [%]");
+            XYChart.Series series = new XYChart.Series();
+            Task fifo = new Task<Void>() {
+
+                @Override
+                protected Void call() throws Exception {
+                    for(int i=0;i<10;i++) {
+                        series.getData().add(new XYChart.Data(i+cycle,Math.sin(i+cycle)));
+                        cycle = 10;
+                    }
+                    while(!isCancelled()) {
+                        series.getData().remove(0);
+                        series.getData().add(new XYChart.Data(cycle,Math.sin(cycle)));
+                        cycle++;
+                        Thread.sleep(666);
+                    }
+                    return null;
+                }
+
+            };
+            line.getData().add(series);
+            new Thread(fifo).start();
         } catch(IOException ioe) {
             System.err.println("IOException caught during start: "+ioe.getLocalizedMessage());
         }
